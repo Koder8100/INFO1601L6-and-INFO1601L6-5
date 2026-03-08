75687018c830376498c170b617e708a7ccc16efd
Kevin Sammy <sammykev.wrk8100@gmail.com>
2026-03-07 20:10:20 -0400

    Uploaded files

---
+++ public/jsreferences.js
@@ -0,0 +1,33 @@
+
+/*
+let bob = {
+  name: 'Bob',
+  balance: 10
+}
+
+function add10(a){
+  a.balance+= 10; // a is a pointer/reference to myObj
+}
+
+console.log(bob.balance);//10
+add10(bob);
+console.log(bob.balance);//20
+
+*/
+
+let obj2 = myobj;
+
+//obj2 and myobj are the same variable
+
+obj2.name = "Shelly";
+
+console.log(myobj.name);//Shelly
+
+//if you need to create a new object and copy its values use Object.assign
+
+let obj3 = {};
+Object.assign(obj3, myobj);
+
+obj3.name = 'Smith';
+
+console.log(myobj.name, obj3.name);//Shelly, Smith
